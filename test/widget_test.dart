a7603dcb6960c769edde1fbd64be959df4538669
adso023 <43103279+adso023@users.noreply.github.com>
2019-12-30 16:16:36 -0500

    Flutter Widget Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_clinic_app/main.dart';
+import 'package:flutter_clinic_app/Login.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

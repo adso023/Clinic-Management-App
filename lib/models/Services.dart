b3cc9921bcb8aa2f2669a4484046832006a91bb9
Adrian D'Souza <dsouzaadrian24@gmail.com>
2020-01-03 23:05:38 -0500

    Updated Fields

--- lib/models/Services.dart
+++ lib/models/Services.dart
@@ -1,39 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class Services {
-  String role;
-  String service;
-  double rate;
-  Image _image;
-
-  final _rolesImages = {
-    'Doctor' : Image.asset('assets/images/DoctorIcon.png', fit: BoxFit.contain,),
-    'Nurse' : Image.asset('assets/images/NurseIcon.png', fit: BoxFit.contain,),
-    'Pharmacy' : Image.asset('assets/images/PharmacyIcon.png', fit: BoxFit.contain,),
-  };
+  String _role;
+  String _service;
+  double _rate;
+  Icon _icon;
 
   factory Services.fromMap({Map<String, dynamic> json}) => Services(role: json['role'], service: json['service'], rate: double.parse(json['rate'].toString()));
 
+  Services({String role, String service, double rate}) : _role = role, _service = service, _rate = rate, _icon = _rolesImages.containsKey(role) ? _rolesImages[role] : Icon(MdiIcons.borderAll);
+
+  Icon get icon => _icon;
+  String get roleName => this._role;
+  set roleName(String value) { this._role = value; }
+  String get serviceName => this._service;
+  set serviceName(String value) { this._service = value; }
+  double get serviceRate => this._rate;
+  set serviceRate(double value) { this._rate = value; }
+
+  static Map<String, Icon> _rolesImages = {
+    'Doctor' : Icon(MdiIcons.doctor, size: 40.0,),
+    'Nurse' : Icon(MdiIcons.motherNurse, size: 40.0,),
+    'Pharmacy' : Icon(MdiIcons.pharmacy, size: 40.0,),
+  };
+
   Map<String, dynamic> toMap() => {'role' : this.roleName, 'service': this.serviceName, 'rate' : this.serviceRate};
-
-  Services({
-    this.role,
-    this.service,
-    this.rate,
-  }) {
-    _image = _rolesImages.containsKey(role) ? _rolesImages[role] : Image.asset('assets/images/BlankImage.png', fit: BoxFit.contain,);
-  }
-
-  Image get image => _image;
-  String get roleName => this.role;
-  set roleName(String value) { this.role = value; }
-  String get serviceName => this.service;
-  set serviceName(String value) { this.service = value; }
-  double get serviceRate => this.rate;
-  set serviceRate(double value) { this.rate = value; }
-
+  
   @override
   String toString() {
-    return "Services (role=$role, service=$service, rate=$rate)";
+    return "Services (role=$roleName, service=$serviceName, rate=$serviceRate)";
   }
 }
